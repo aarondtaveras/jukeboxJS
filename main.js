7904a4e7f662464d44f3412b710bbd5fd9cfab8a
adt <aarondtaveras@gmail.com>
2018-06-01 17:25:07 -0400

    Added loadSong method and some project documentation.

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
+/**	
+	Jukebox in JS: 6/1/18 
+	Author: Aaron Taveras 
+
+	Assignment Description: Display at least one song on the
+	page when the page loads Give the user the ability to play
+	that song, without using the "built-in" play button. This
+	could be through a different button, through clicking or
+	mousing over an image on the page, or any other device of 
+	your choosing. Give the user the ability to stop that song
+	without using the "built-in" stop button. Once again, this
+	could be through a different button, through clicking or 
+	mousing over an image on the page, or any other device of 
+	your choosing. Give the user the ability to load at least
+	one different song into the Jukebox besides the one that is
+	loaded when the page initially renders. The whole Jukebox 
+	should be backed by an object called Jukebox with methods
+	to play, stop, and load songs.
+
+**/
+
 document
+
+// ************************ CLASS DEFINITIONS ************************ 
+
+// Represents a song as strings of text. 
 
 class song{
 	constructor(name,mp3){
@@ -7,7 +32,8 @@ class song{
 	}
 }
 
-
+// Encapsulates an audio player with play, pause, skip, shuffle, and
+// functionality to play songs in any desired order.  
 class Jukebox{
 	constructor(){
 		this.playlist = [];
@@ -46,7 +72,20 @@ class Jukebox{
 		this.goToSong(pos);
 	}
 
+	loadSong(song){
+		this.playlist.push(song);
+	}
+
 }
+
+/** 
+	Here we create our objects, variables, and eventListeners that will be used
+	in our program. We will need a Jukebox object to operate on, 
+	buttons and listeners for each of the buttons, and a listener 
+	for our form in which we add new mp3s to the playlist. 
+**/ 
+
+// ************************ VARIABLES ************************ 
 
 var myJukebox = new Jukebox();
 
@@ -55,6 +94,9 @@ var playButton = document.getElementById("playbutton");
 var shuffleButton = document.getElementById("shufflebutton");
 var nextButton = document.getElementById("nextbutton");
 var prevButton = document.getElementById("prevbutton");
+var submitButton = document.getElementById("submitbutton");
+
+// ************************ LISTENERS ************************
 
 myJukebox.audioPlayer.addEventListener("ended",function(event){
 	
@@ -91,6 +133,10 @@ prevButton.addEventListener("click", function(event){
 				myJukebox.goToSong(i+1);
 		}
 	}
+})
+
+submitButton.addEventListener("click", function(event){
+	myJukebox.loadSong(new song("imported_song",document.getElementById(url)));
 })
 
 
